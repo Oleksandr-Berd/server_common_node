8883d5c7b257348382bc5c4d941a9436ce6f8b55
Oleksandr-Berd <alex.berd86@gmail.com>
2023-07-10 14:53:05 +0300

    delete route

--- controllers/entertainment/moviesControllers.js
+++ controllers/entertainment/moviesControllers.js
@@ -21,7 +21,6 @@ const getOneById = async (req, res) => {
 
 const add = async (req, res) => {
   const movie = await Movies.create({ ...req.body });
-console.log("test".red, req);
   if (!movie) {
     res.status(400);
     throw new Error("Unable to save in a data base");
@@ -30,8 +29,23 @@ console.log("test".red, req);
     .status(201)
     .json({ code: 201, message: "Successful success", data: movie });
 };
+
+const removeById = async (req, res) => {
+    const { id } = req.params;
+    
+    console.log("test".red, id);
+     const result = await Movies.findByIdAndRemove(id);
+     if (!result) {
+       throw HttpError(404, "Not found");
+     }
+     res.status(200).json({
+       message: "Delete is successful",
+     });
+}
+
 module.exports = {
   getAll: ctrlWrapper(getAll),
   getOneById: ctrlWrapper(getOneById),
   add: ctrlWrapper(add),
+  removeById: ctrlWrapper(removeById),
 };
--- routes/entertainment/moviesRoutes.js
+++ routes/entertainment/moviesRoutes.js
@@ -8,5 +8,7 @@ const moviesRouter = express.Router();
 moviesRouter.get("/movies", ctrlMovies.getAll)
 moviesRouter.get("/movies/:id", ctrlMovies.getOneById);
 moviesRouter.post("/movies", ctrlMovies.add);
+moviesRouter.delete("/movies/:id", ctrlMovies.removeById);
+
 
 module.exports = moviesRouter;
