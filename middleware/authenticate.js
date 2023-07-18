1f29aac33db5d2ccd572d069a12deeb428c8a074
Oleksandr-Berd <alex.berd86@gmail.com>
2023-07-18 10:39:06 +0300

    just consoling

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -8,29 +8,22 @@ const { SECRET_KEY } = process.env;
 const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
     const [bearer, token] = authorization.split(" ");
-    
   if (bearer !== "Bearer") {
     next(HttpError(401));
   }
 
   try {
       const { id, email } = jwt.verify(token, SECRET_KEY);
-      
+     
      const user =
-       !!id
+       id
          ? await User.findById(id)
              : await User.findOne({ email });
-      console.log(
-        "user:",
-        user,
-        "user.token: ",
-        user.token,
-        "user.token !== token",
-        user.token !== token
-      );
+     
     if (!user || !user.token || user.token !== token) {
           next(HttpError(401));
     }
+      
 req.user = user
       next()
   } catch (error) {
