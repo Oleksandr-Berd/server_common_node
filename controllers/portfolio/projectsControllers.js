50b5a1b5853d2767f9190d3d80f9294ab67b8350
Oleksandr-Berd <alex.berd86@gmail.com>
2023-07-29 09:33:30 +0300

    made filter projects's difficulties

--- controllers/portfolio/projectsControllers.js
+++ controllers/portfolio/projectsControllers.js
@@ -5,27 +5,35 @@ const { Projects } = require("../../models/index");
 const { ctrlWrapper, HttpError } = require("./../../utils/index");
 
 const getAll = async (req, res) => {
-   const { page = 1, limit = 1000 } = req.query;
-   const skip = (page - 1) * limit;
+  const { page = 1, limit = 1000 } = req.query;
+  const skip = (page - 1) * limit;
+  const difficulty = req.query;
 
-   const result = await Projects.find({}, "", { skip, limit });
-   res.status(200).json(result);
+console.log(difficulty);
+
+   const result =
+     difficulty !== "Get All"
+       ? await Projects.find({ difficulty: difficulty }, "", { skip, limit })
+       : await Projects.find({}, "", { skip, limit });
+
+  
+console.log(result);
+  
+  res.status(200).json(result);
 };
 
 const getAllByDifficulty = async (req, res) => {};
 
 const getDetails = async (req, res) => {
+  const { title } = req.params;
 
-  const { title } = req.params
-  
-  const result = await Projects.find({title});
+  const result = await Projects.find({ title });
 
-   res.status(200).json(result);
+  res.status(200).json(result);
 };
 
 const addNew = async (req, res) => {
-
-const {title} = req.body
+  const { title } = req.body;
 
   const project = await Projects.create({ ...req.body });
 
@@ -33,27 +41,26 @@ const {title} = req.body
     throw HttpError(400, "Unable to save your data");
   }
 
-  const coverUrl = gravatar.url(title);  
+  const coverUrl = gravatar.url(title);
 
-  res
-    .status(201)
-    .json({
-      code: 201,
-      message: "Successful success",
-      data: { ...project, coverImage: coverUrl },
-    });
+  res.status(201).json({
+    code: 201,
+    message: "Successful success",
+    data: { ...project, coverImage: coverUrl },
+  });
 };
 
-const updateOne = async (req, res) => { };
+const updateOne = async (req, res) => {};
 
 const updateCover = async (req, res) => {
-  const { title } = req.body
-  
-  
+  const { title } = req.body;
+
   const data = req.file.path;
 
-
-  const result = await Projects.findOneAndUpdate({ title }, {coverImage: data})
+  const result = await Projects.findOneAndUpdate(
+    { title },
+    { coverImage: data }
+  );
 
   if (!result) {
     throw HttpError(404, "Not found");
@@ -63,30 +70,29 @@ const updateCover = async (req, res) => {
 };
 
 const updatePreview = async (req, res) => {
-   const { title } = req.body;
+  const { title } = req.body;
 
   const data = req.file.path;
-  
-  const project = await Projects.find({ title })
-  
-  const {preview} = project[0]
+
+  const project = await Projects.find({ title });
+
+  const { preview } = project[0];
 
   preview.push(data);
 
+  const test = (req, res) => {};
 
-const test = (req, res) => {}
+  const result = await Projects.findOneAndUpdate(
+    { title },
+    { preview: preview }
+  );
 
-   const result = await Projects.findOneAndUpdate(
-     { title },
-     { preview: preview }
-   );
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
 
-   if (!result) {
-     throw HttpError(404, "Not found");
-   }
-
-   res.status(201).json({ data });
-}
+  res.status(201).json({ data });
+};
 
 const removeOne = async (req, res) => {};
 
@@ -98,5 +104,5 @@ module.exports = {
   updateOne: ctrlWrapper(updateOne),
   removeOne: ctrlWrapper(removeOne),
   updateCover: ctrlWrapper(updateCover),
-  updatePreview: ctrlWrapper(updatePreview), 
+  updatePreview: ctrlWrapper(updatePreview),
 };
