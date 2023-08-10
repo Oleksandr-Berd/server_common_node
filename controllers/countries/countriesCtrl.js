10d36a512471aa4418b51560a7ba456aab089b6e
Oleksandr-Berd <alex.berd86@gmail.com>
2023-08-10 14:23:10 +0300

    add total page for getAll countries

--- controllers/countries/countriesCtrl.js
+++ controllers/countries/countriesCtrl.js
@@ -6,9 +6,13 @@ const getAll = async (req, res) => {
   const { page = 1, limit = 8 } = req.query;
   const skip = (page - 1) * limit;
 
+const allCountries = await Countries.find({}, "");
+    
+  const totalPages = Math.ceil(allCountries.length / limit); 
+    
     const result = await Countries.find({}, "", { skip, limit });
     
-  res.status(200).json(result);
+  res.status(200).json({result, totalPages});
 };
 
 
