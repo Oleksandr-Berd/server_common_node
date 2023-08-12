f2446437b6e2d10a769fbb76ada49bd818eadbe6
Oleksandr-Berd <alex.berd86@gmail.com>
2023-08-12 12:33:10 +0300

    regions filter and fix total page

--- controllers/countries/countriesCtrl.js
+++ controllers/countries/countriesCtrl.js
@@ -3,21 +3,43 @@ const { Countries } = require("../../models/index")
 const { ctrlWrapper, HttpError } = require("./../../utils/index");
 
 const getAll = async (req, res) => {
-  const { page = 1, limit = 8, searchQuery } = req.query;
+  const { page = 1, limit = 8, searchQuery, region } = req.query;
   const skip = (page - 1) * limit;
 
-  const allCountries = await Countries.find({}, "");
+const filterRegion = region === "all" ? null : region
+    
+  const allCountries =
+    searchQuery && !filterRegion
+      ? await Countries.find({ name: new RegExp(searchQuery, "i") }, "")
+      : filterRegion && !searchQuery
+      ? await Countries.find({ region: filterRegion }, "")
+      : filterRegion && searchQuery
+      ? await Countries.find(
+          { region: filterRegion, name: new RegExp(searchQuery, "i") },
+          "",
+          { skip, limit }
+        )
+      : await Countries.find({}, "");
 
-  const result = searchQuery
-    ? await Countries.find({ name: new RegExp(searchQuery, "i") }, "", {
-        skip,
-        limit,
-      })
-    : await Countries.find({}, "", { skip, limit });
+  const result =
+    searchQuery && !filterRegion
+      ? await Countries.find({ name: new RegExp(searchQuery, "i") }, "", {
+          skip,
+          limit,
+        })
+      : filterRegion && !searchQuery
+      ? await Countries.find({ region: filterRegion }, "", { skip, limit })
+      : filterRegion && searchQuery
+      ? await Countries.find(
+          { region: filterRegion, name: new RegExp(searchQuery, "i") },
+          "",
+          { skip, limit }
+        )
+      : await Countries.find({}, "", { skip, limit });
 
-  const totalPages = searchQuery
-    ? Math.ceil(result.length / limit)
-    : Math.ceil(allCountries.length / limit);
+    
+  const totalPages =
+     Math.ceil(allCountries.length / limit);
     
   res.status(200).json({ result, totalPages });
 };
