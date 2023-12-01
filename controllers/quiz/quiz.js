b04cdc24042041ea879e507250271b50d2d4cc6c
Oleksandr-Berd <alex.berd86@gmail.com>
2023-12-01 18:24:56 +0200

    upload icons quizz

--- controllers/quiz/quiz.js
+++ controllers/quiz/quiz.js
@@ -1,14 +1,11 @@
-const { QuizCollection } = require("../../models/index")
+const { QuizCollection } = require("../../models/index");
 const { ctrlWrapper, HttpError } = require("./../../utils/index");
 
-
 const getAll = async (req, res) => {
-    
-    const result = await QuizCollection.find({}, "");
+  const result = await QuizCollection.find({}, "");
 
-
-    res.status(200).json(result)
-}
+  res.status(200).json(result);
+};
 
 const getOneById = async (req, res) => {
   const { id } = req.params;
@@ -20,8 +17,23 @@ const getOneById = async (req, res) => {
   res.status(200).json(result);
 };
 
+const updateIcon = async (req, res) => {
+  const { title } = req.body;
+
+  const data = req.file.path;
+
+  const result = await QuizCollection.findOneAndUpdate({ title }, { icon: data });
+  
+  console.log(result);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+
+  res.status(201).json({ data });
+};
+
 module.exports = {
   getAll: ctrlWrapper(getAll),
   getOneById: ctrlWrapper(getOneById),
+  updateIcon: ctrlWrapper(updateIcon),
 };
-
--- routes/quiz/quizRoutes.js
+++ routes/quiz/quizRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express")
 
 const ctrlQuiz = require("../../controllers/quiz/quiz")
+const {uploadCloud} = require("../../middleware/index") 
 
 const quizRouter = express.Router()
 
 quizRouter.get("/all", ctrlQuiz.getAll)
 quizRouter.get("/:id", ctrlQuiz.getOneById);
 
+quizRouter.patch("/icon", uploadCloud.single("icon"), ctrlQuiz.updateIcon);
 
 module.exports = quizRouter;
