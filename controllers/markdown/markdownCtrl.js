a9daa8a3bae8585a3b1e07336d16aa789ba95c5d
Oleksandr-Berd <alex.berd86@gmail.com>
2023-12-11 14:23:56 +0200

    add getOne

--- controllers/markdown/markdownCtrl.js
+++ controllers/markdown/markdownCtrl.js
@@ -1,16 +1,22 @@
-const { MarkdownModel } = require("../../models/index.js"); 
+const { MarkdownModel } = require("../../models/index.js");
 
 const { ctrlWrapper, HttpError } = require("./../../utils/index");
 
 const getAll = async (req, res) => {
-
-    console.log(MarkdownModel);
   const result = await MarkdownModel.find({}, "");
 
-    
+  res.status(200).json(result);
+};
+
+const getOne = async (req, res) => {
+  const { name } = req.params;
+
+  const result = await MarkdownModel.find({ name: name }, "");
+
   res.status(200).json(result);
 };
 
 module.exports = {
   getAll: ctrlWrapper(getAll),
+  getOne: ctrlWrapper(getOne),
 };
--- routes/markdown/markdownRoutes.js
+++ routes/markdown/markdownRoutes.js
@@ -6,6 +6,8 @@ const { uploadCloud } = require("../../middleware/index");
 const markdownRouter = express.Router();
 
 markdownRouter.get("/all", ctrlMarkdown.getAll);
+markdownRouter.get("/:name", ctrlMarkdown.getOne);
+
 // markdownRouter.get("/:name", ctrlMarkdown.getOne);
 
 module.exports = markdownRouter;
