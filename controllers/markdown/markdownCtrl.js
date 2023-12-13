cfdab039ee4db4f59b19410f34915d0321909d21
Oleksandr-Berd <alex.berd86@gmail.com>
2023-12-13 19:14:19 +0200

    made sort

--- controllers/markdown/markdownCtrl.js
+++ controllers/markdown/markdownCtrl.js
@@ -5,7 +5,13 @@ const { ctrlWrapper, HttpError } = require("./../../utils/index");
 const getAll = async (req, res) => {
   const result = await MarkdownModel.find({}, "");
 
-  res.status(200).json(result);
+  const welcomeDoc = result.find(({ name }) => name === "welcome.md");
+
+const sortedResult = result.sort((a, b) => b.createdAt - a.createdAt)
+
+  const uniquesResult = sortedResult.filter(({name}) => name !== "welcome.md");
+  
+  res.status(200).json([welcomeDoc, ...uniquesResult]);
 };
 
 const getOne = async (req, res) => {
@@ -17,41 +23,37 @@ const getOne = async (req, res) => {
 };
 
 const addNew = async (req, res) => {
-const {name} = req.body
+  const { name } = req.body;
 
   const isMatch = await MarkdownModel.find({ name: name }, "");
 
   if (isMatch.length) {
-    throw HttpError(409, `The document with name ${name} already exists`)
+    throw HttpError(409, `The document with name ${name} already exists`);
   }
   const newDoc = await MarkdownModel.create({ ...req.body });
   if (!newDoc) {
     throw HttpError(400, "Unable to save your data");
   }
 
- 
-    
   res.status(201).json({
     code: 201,
     message: "Successful success",
-    data: { ...newDoc},
+    data: { ...newDoc },
   });
 };
 
- const removeByName = async (req, res) => {
-   const { name } = req.params;
+const removeByName = async (req, res) => {
+  const { name } = req.params;
 
-   const result = await MarkdownModel.findOneAndRemove({name:name});
+  const result = await MarkdownModel.findOneAndRemove({ name: name });
 
-   if (!result) {
-     throw HttpError(404, "Not found");
-   }
-   res.status(200).json(
-     {
-       message: "Delete is successful",
-     }
-   );
- };
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.status(200).json({
+    message: "Delete is successful",
+  });
+};
 
 module.exports = {
   getAll: ctrlWrapper(getAll),
