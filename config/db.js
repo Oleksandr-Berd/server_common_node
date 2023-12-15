a478e9a83d83fd7189351a639d74198dcbd3527e
Oleksandr-Berd <alex.berd86@gmail.com>
2023-12-15 07:05:55 +0200

    try to sort

--- config/db.js
+++ config/db.js
@@ -1,21 +1,28 @@
 const mongoose = require("mongoose");
 const { Projects } = require("../models");
 
-
 const connectDB = async () => {
   try {
     mongoose.set("strictQuery", true);
     const db = await mongoose.connect(process.env.DB_URI);
-    const sortedDocuments = await Projects.find().sort({ createdAt: 1 });
+
+    // Fetch documents and sort by creation date in descending order
+    const sortedDocuments = await Projects.find().sort({ createdAt: -1 });
     console.log(
-      `Successful success with DB, name: ${db.connection.name} on port: ${db.connection.port}, on host: ${db.connection.host}`
+      `Successful connection to DB, name: ${db.connection.name} on port: ${db.connection.port}, on host: ${db.connection.host}`
         .bold.brightCyan.italic
     );
+
+    // Log the sorted documents for verification
+    console.log(sortedDocuments);
+
+    // Return the sorted documents if needed
+    return sortedDocuments;
   } catch (error) {
     console.error(
       `There is some error occur: ${error.message}`.bold.red.italic
     );
   }
-}
+};
 
-module.exports = connectDB
+module.exports = connectDB;
