c3fd6d95e3a9a40f14371f74ea251537271d39b5
Oleksandr-Berd <alex.berd86@gmail.com>
2023-12-15 07:02:00 +0200

    try to sort

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
   try {
     mongoose.set("strictQuery", true);
     const db = await mongoose.connect(process.env.DB_URI);
-    const sortedDocuments = await Projects.find().sort({ createdAt: -1 });
+    const sortedDocuments = await Projects.find().sort({ createdAt: 1 });
     console.log(
       `Successful success with DB, name: ${db.connection.name} on port: ${db.connection.port}, on host: ${db.connection.host}`
         .bold.brightCyan.italic
